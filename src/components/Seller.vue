93d1b7807a69eda3a19e584b5cbb06928dfb05a6
Trevor Spitzley <spitzltr@mail.gvsu.edu>
2021-04-13 21:16:55 -0400

    Architecture of Owner.vue begun.

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -1,22 +1,42 @@
 <template>
-  <div>
+  <div id="contents">
+    <div id="header">
+      <h2>Insert centered name here</h2>
+    </div>
     <button v-on:click="goBack()">Go Back</button>
+    <AddCar></AddCar>
+    <Reservations></Reservations>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import { FirebaseFirestore } from "@firebase/firestore-types";
 
-@Component
-export default class Seller extends Vue{
-    $router: any;
+import { QueryDocumentSnapshot } from "@firebase/firestore-types";
+import { QuerySnapshot } from "@firebase/firestore-types";
 
-  goBack(): void{
+import { FirebaseAuth } from "@firebase/auth-types";
+import "firebase/auth";
+
+import AddCar from "./AddCar.vue";
+import Reservations from './Reservations.vue';
+
+@Component({
+  components: {
+    AddCar, Reservations,
+  },
+})
+
+export default class Seller extends Vue {
+  readonly $appDB!: FirebaseFirestore;
+  readonly $appAuth!: FirebaseAuth;
+  $router: any;
+
+  goBack(): void {
     this.$router.back();
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import firebase from "firebase/app"
-// import "firebase/firestore"
-// import "firebase/auth"
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
 import { AppRouter } from "./app-routing";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoRbbYkzZCcbrQ7zxQNrEtzjKhmWWofGQ",
+  authDomain: "finalproject-9d7cf.firebaseapp.com",
+  projectId: "finalproject-9d7cf",
+  storageBucket: "finalproject-9d7cf.appspot.com",
+  messagingSenderId: "434244504049",
+  appId: "1:434244504049:web:226387663860147bb25c50",
+  measurementId: "G-6D9BEKNY8Q"
+};
 
 Vue.config.productionTip = false
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-// Vue.prototype.$appDB = firebase.firestore();
-// Vue.prototype.$appAuth = firebase.auth();
+Vue.prototype.$appDB = firebase.firestore();
+Vue.prototype.$appAuth = firebase.auth();
 
 
 new Vue({
